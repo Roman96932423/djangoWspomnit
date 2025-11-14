6278854c5aca458f21f47d3dc912df34630e02cd
Roman96932423 <ddent358@gmail.com>
2025-11-14 12:49:06 +0100

    Post update view

--- blog/web/urls.py
+++ blog/web/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, UpdateView
 
 from web.views import UserBlogView
 from web.models import Post
@@ -13,5 +13,12 @@ urlpatterns = [
     path('<int:pk>/', UserBlogView.as_view(), name='user_posts'),
     
     # Strona detalnej treści wskazanego posta
-    path('post-detail/<int:pk>/', DetailView.as_view(model=Post), name='post_detail')
+    path('post-detail/<int:pk>/', DetailView.as_view(model=Post), name='post_detail'),
+    
+    # Strona do edytowanie posta
+    path('post-update/<int:pk>/', UpdateView.as_view(
+        model=Post,
+        fields=('title', 'teaser', 'content', 'header_image'),
+        success_url='/post-detail/{id}'
+        ), name='post_update')
 ]
